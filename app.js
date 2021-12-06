707bec55750ece487ba25ea15c91784f5c07d2a0
yannnii <81016075+yannnii@users.noreply.github.com>
2021-12-06 19:57:03 +0400

    add auth and captcha

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 
 app.use(multer({dest:"upload"}).single("img"));
 
-function add_user(login, email, password, img)
+function add_user(login, email, password, img, request, response)
 {
   const db = new sqlite.Database('reg.db', sqlite.OPEN_READWRITE, (err) =>
   {
@@ -28,8 +28,15 @@ function add_user(login, email, password, img)
   let QueryData = {$name: login, $Em: email, $pw: password, $img: img};
   db.run(insert_user, QueryData, (err) =>{
       if (err)
-          return console.error(err.message);
-  console.log("Пользователь добавлен");
+      {
+      	response.sendFile(__dirname + "/public/error_user.html");
+        return console.error(err.message);
+      }
+      else
+      {
+      	response.sendFile(__dirname + "/public/success_user.html");
+  		console.log("Пользователь добавлен");
+	  }
   });
   db.close((err) =>{
     if(err)
@@ -76,6 +83,43 @@ function get_question()
   });
 }
 
+function auth(login, password, request, response)
+{
+  const db = new sqlite.Database('reg.db', sqlite.OPEN_READWRITE, (err) =>
+  {
+      if(err)
+          return console.error(err.message);
+      console.log("Успешное подключение к БД");
+  } );
+  const select_user=`SELECT * FROM Users WHERE login = $login AND password = $password`;
+  let QueryData = {$login: login, $password: password};
+  db.all(select_user, QueryData, (err, result) =>{
+      if (err)
+      {
+      	response.sendFile(__dirname + "/public/error_auth.html");
+        return console.error(err.message);
+      }
+      else
+      {
+      	console.log("Запрос выполнен успешно");
+      	dataRes = JSON.parse(JSON.stringify(result));
+      	if (dataRes=="")
+      	{
+      		response.sendFile(__dirname + "/public/error_auth.html");
+      	}
+      	else 
+      	{
+      		//console.log(dataRes);
+      		response.render('\success_auth', {nick:  login});
+      	}
+      }
+  db.close((err) =>{
+    if(err)
+    return console.error(err.message);
+  });
+  });
+}
+
 function createServer(){
   
   app.get("/index.html", function (request, response) {
@@ -88,8 +132,11 @@ function createServer(){
       response.sendFile(__dirname + "/public/question.html");
   });
   app.get("/question_read.html", function (request, response) {
-    console.log( dataRes);
+      get_question();
       response.render('\question_read', {dataRes:  dataRes});
+  });
+  app.get("/auth.html", function (request, response) {
+      response.sendFile(__dirname + "/public/auth.html");
   });
 
   app.post("", urlencodedParser, function (request, response) {
@@ -98,9 +145,7 @@ function createServer(){
             let email = request.body.email;
             let password = request.body.password;
             let img = request.file;
-            console.log(img);
-            add_user(login, email, password, img.filename);
-            response.sendFile(__dirname + "/public/success_user.html");
+            add_user(login, email, password, img.filename, request, response);
         });
   app.post("/index.html", urlencodedParser, function (request, response) {
       if(!request.body) return response.sendStatus(400);
@@ -108,8 +153,7 @@ function createServer(){
             let email = request.body.email;
             let password = request.body.password;
             let img = request.file;
-            add_user(login, email, password, img.filename);
-            response.sendFile(__dirname + "/public/success_user.html");
+            add_user(login, email, password, img.filename, request, response);
         });
     app.post("/question.html", urlencodedParser, function (request, response) {
         if(!request.body) return response.sendStatus(400);
@@ -119,6 +163,12 @@ function createServer(){
           add_question(email, question, img.filename);
           response.sendFile(__dirname + "/public/success_question.html");
   });
+    app.post("/auth.html", urlencodedParser, function (request, response) {
+      if(!request.body) return response.sendStatus(400);
+            let login = request.body.login;
+            let password = request.body.password;
+            auth(login, password, request, response);
+        });
 }
 
 createServer();
